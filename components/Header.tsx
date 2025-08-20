29d281858452566d3524bdecb5e84462a552ad69
Beom Mee <whitebigbabybird@gmail.com>
2025-08-20 17:54:55 +0900

    Define components inside Header
    which are SignedIn and SignedOut components and children belonging to them respectively
    
    add styles classifying if it is signed in or not with usePathname hook.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,42 @@
-"use strict"; // make this a client component due to check in where we are
+"use client"; // make this a client component due to check in where we are
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Shield } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const pathname = usePathname(); // needs to use "use client" due to checking path
+  const isHomepage = pathname === "/";
+
   return (
-    <div>
-      <h1>Header</h1>
+    <div
+      className={`p-4 flex items-center justify-between ${isHomepage ? "bg-sky-100" : "bg-white border-b-2 border-sky-100"}`}
+    >
+      <Link href="/" className="flex items-center">
+        <Shield className="h-6 w-6 text-sky-400 mr-2" />
+        <h1 className="text-xl font-semibold text-sky-400">Expensio</h1>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href={"/receipts"}>
+            <Button variant={"ghost"}>My receipts</Button>
+          </Link>
+
+          <Link href={"/manage-plan"}>
+            <Button>Manage Plan</Button>
+          </Link>
+
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Login</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </div>
   );
 }
