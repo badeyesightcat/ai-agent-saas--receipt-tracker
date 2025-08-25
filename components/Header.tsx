de8e8801c4a4195e7078e1f6eff2385d1bfd2548
Beom Mee <whitebigbabybird@gmail.com>
2025-08-25 17:38:00 +0900

    Modify the use of ui class defintion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 "use client"; // make this a client component due to check in where we are
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Shield } from "lucide-react";
 import Link from "next/link";
@@ -9,14 +10,13 @@ import { cn } from "@/lib/utils";
 function Header() {
   const pathname = usePathname(); // needs to use "use client" due to checking path
   const isHomepage = pathname === "/";
+  const styleClasses = cn(
+    "p-4 flex items-center justify-between",
+    isHomepage ? "bg-sky-100" : "bg-white border-b-2 border-sky-100",
+  );
 
   return (
-    <div
-      className={cn("p-4 flex items-center justify-between", {
-        "bg-sky-100": isHomepage,
-        "bg-white border-b-2 border-sky-100": !isHomepage,
-      })}
-    >
+    <div className={styleClasses}>
       <Link href="/" className="flex items-center">
         <Shield className="h-6 w-6 text-sky-400 mr-2" />
         <h1 className="text-xl font-semibold text-sky-400">Expensio</h1>
