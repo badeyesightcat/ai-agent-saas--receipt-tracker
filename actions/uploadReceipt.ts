3cf97463721451df969ecc7f3c3631c200b4628e
Beom Mee <whitebigbabybird@gmail.com>
2025-08-26 17:40:23 +0900

    Add upload functionality for receipt files with Convex storage integration

--- actions/uploadReceipt.ts
+++ actions/uploadReceipt.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { api } from "@/convex/_generated/api";
+import convex from "@/lib/convexClient";
 import { currentUser } from "@clerk/nextjs/server";
+import { getFileDownloadUrl } from "./getFileDownloadUrl";
 
 function isAllowedFile(file: File): boolean {
   const allowedExtensions = /\.(jpg|jpeg|png|gif)$/i;
@@ -33,6 +36,58 @@ export async function uploadReceipt(formData: FormData) {
         error: "Invalid file type. Only image files are allowed.",
       };
     }
+
+    // Get upload URL from Convex
+    const uploadUrl = await convex.mutation(api.receipts.generateUploadUrl, {});
+
+    // Convert the file to arrayBuffer for fetch API
+    // [!important]
+    // for uploading only, blob is Okay,
+    // for using meta data and contents inside files, arraybuffer is needed
+    const arrayBuffer = await file.arrayBuffer();
+
+    // Upload the file to Convex storage
+    const uploadResponse = await fetch(uploadUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": file.type,
+      },
+      // [!important] Why not use arrayBuffer directly?
+      // Because fetch API expects body to be of
+      // type Blob, BufferSource, FormData, URLSearchParams, ReadableStream, or USVString.
+      // arrayBuffer is of type ArrayBuffer which is not directly accepted.
+      // So we convert it to Uint8Array which is a BufferSource.
+      body: new Uint8Array(arrayBuffer),
+    });
+
+    if (!uploadResponse.ok) {
+      throw new Error(`Failed to upload file: ${uploadResponse.statusText}`);
+    }
+
+    // Get the storage ID from the response
+    const { storageId } = await uploadResponse.json();
+
+    // Add receipt to the database
+    const receiptId = await convex.mutation(api.receipts.storeReceipt, {
+      userId: user.id,
+      fileId: storageId,
+      fileName: file.name,
+      size: file.size,
+      mimeType: file.type,
+      // fileDisplayName: formData.get("fileDisplayName") as string | null,
+    });
+
+    // Generate the file URL
+    const fileUrl = await getFileDownloadUrl(storageId);
+
+    // if (!fileUrl.success) {
+    //   throw new Error(fileUrl.error || "Failed to generate file URL");
+    // }
+
+    // [TODO] Trigger inngest agent flow
+    // ...
+
+    return { success: true, data: { receiptId, fileName: file.name } }; //  fileUrl: fileUrl.downloadUrl
   } catch (error) {
     console.error("Server action upload error:", error);
 
