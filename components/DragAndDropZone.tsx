b7ddb86a90c7c96f99863d656b7e55b2a3e5efd4
Beom Mee <whitebigbabybird@gmail.com>
2025-08-27 15:19:46 +0900

    Change upload spinner color from indigo to blue in DragAndDropZone component

--- components/DragAndDropZone.tsx
+++ components/DragAndDropZone.tsx
@@ -157,7 +157,7 @@ function DragAndDropZone() {
         >
           {isUploading ? (
             <div className="flex flex-col items-center">
-              <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-indigo-500 mb-2"></div>
+              <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500 mb-2"></div>
               <p>Uploading...</p>
             </div>
           ) : !isUserSignedIn ? (
