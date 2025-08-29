ee8f2602eeb4bcd76867e0d4b3e7674bdb7a19a8
Beom Mee <whitebigbabybird@gmail.com>
2025-08-29 18:08:45 +0900

    Add Events enum to define receipt processing events

---
+++ inngest/constants.ts
@@ -0,0 +1,11 @@
+export enum Events {
+  //   RECEIPT_SCANNED = "receipt.scanned",
+  //   RECEIPT_PROCESSED = "receipt.processed",
+  //   RECEIPT_ANALYSIS_FAILED = "receipt.analysis.failed",
+  EXTRACT_DATA_FROM_RECEIPT_AND_SAVE_TO_DATABASE = "extract-data-from-receipt-and-save-to-database",
+}
+
+// export enum FunctionId {
+//   PROCESS_RECEIPT = "process-receipt",
+//   ANALYZE_RECEIPT = "analyze-receipt",
+// }
