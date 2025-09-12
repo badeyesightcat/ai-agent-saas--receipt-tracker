7ca4b9e746d43417f5ece86b3cad25b09eb38f1a
Beom Mee <whitebigbabybird@gmail.com>
2025-09-12 21:15:52 +0900

    Update layout in Receipts component to improve grid structure and responsiveness

--- app/receipts/page.tsx
+++ app/receipts/page.tsx
@@ -4,9 +4,13 @@ import ReceiptList from "@/components/ReceiptList";
 function Receipts() {
   return (
     <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-        <DragAndDropZone />
-        <ReceiptList />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <div className="col-span-1 lg:col-span-1">
+          <DragAndDropZone />
+        </div>
+        <div className="col-span-1 lg:col-span-2">
+          <ReceiptList />
+        </div>
       </div>
     </div>
   );
