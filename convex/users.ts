77bc809daea8490384b3c1dc1b18f49438c6a2a8
Beom Mee <whitebigbabybird@gmail.com>
2025-09-30 19:50:30 +0900

    feat: Add functions for users table and make use of them inside User page

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const getUsers = query({
   args: {},
@@ -6,5 +7,17 @@ export const getUsers = query({
     const users = await ctx.db.query("users").collect();
 
     return users;
+  },
+});
+
+export const add = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await ctx.db.insert("users", {
+      name: args.name,
+    });
+    return userId;
   },
 });
